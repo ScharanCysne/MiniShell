c77fd3b6c939e055b972d2d09c08b54b03731e10
nicholas-scharan <nicholas_scharan@mckinsey.com>
2024-06-04 14:00:57 -0300

    Adding REPL

--- app/main.py
+++ app/main.py
@@ -5,14 +5,15 @@ def main():
     # List of valid commands
     commands = ["ls", "echo", "mkdir"]
 
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
+        # Wait for user input
+        command = input()
 
-    if command not in commands:
-        sys.stdout.write(f"{command}: command not found\n")
+        if command not in commands:
+            sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
